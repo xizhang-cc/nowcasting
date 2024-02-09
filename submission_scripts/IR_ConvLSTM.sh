2087696144423658ae1e1583a7b5ff1773ea1347
zhang2012 <zhang2012@fit.edu@ai-panther.fit.edu>
2024-02-08 21:32:36 -0500

    ir only runs on hpc

--- submission_scripts/IR_ConvLSTM.sh
+++ submission_scripts/IR_ConvLSTM.sh
@@ -5,14 +5,14 @@
 
 #SBATCH --nodes 1
 #SBATCH --ntasks 1
-#SBATCH --mem=GB
-#SBATCH --partition=short
+#SBATCH --mem=256GB
+#SBATCH --partition=gpu1
 
 #SBATCH --error=/home1/zhang2012/nowcasting/runs/job.%J.err 
 #SBATCH --output=/home1/zhang2012/nowcasting/runs/job.%J.out
 
 
-python /home1/zhang2012/nowcasting/servir/datasets/dataLoader_wa_IR.py
+# python /home1/zhang2012/nowcasting/servir/datasets/dataLoader_wa_IR.py
 
-# export CUDA_VISIBLE_DEVICES=3
-# python /home1/zhang2012/nowcasting/examples/wa_IR_convLSTM.py
+export CUDA_VISIBLE_DEVICES=2
+python /home1/zhang2012/nowcasting/examples/wa_IR_convLSTM.py
