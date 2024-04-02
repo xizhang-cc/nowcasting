4457c43261d29317988f0c11a275dab48da9f8d4
zhang2012 <zhang2012@fit.edu@ai-panther.fit.edu>
2024-04-01 20:09:56 -0400

    updates

--- submission_scripts/run_exp.sh
+++ submission_scripts/run_exp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name ghana_imerg
+#SBATCH --job-name q0.95
 
 
 #SBATCH --nodes 1
@@ -11,5 +11,5 @@
 #SBATCH --error=/home1/zhang2012/nowcasting/runs/job.%J.err 
 #SBATCH --output=/home1/zhang2012/nowcasting/runs/job.%J.out
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 python /home1/zhang2012/nowcasting/examples/exp_1dataset.py
