b3fcf29e607ca2b58d98f624f25f817aa92c3bf4
Xi Zhang <cc.zhang1992@gmail.com>
2024-09-10 02:21:23 -0400

    updates

--- examples/metric_plots.py
+++ examples/metric_plots.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib import gridspec
 
 # load the csv file
-csv_file = '/home/cc/projects/nowcasting/lightning_logs/version_26/metrics.csv'
+csv_file = '/home/cc/projects/nowcasting/lightning_logs/metrics_42720.csv'
 
 df = pd.read_csv(csv_file)
 
@@ -17,6 +17,6 @@ gs = gridspec.GridSpec(1, 1)
 ax = plt.subplot(gs[0])
 
 # ax.plot(df['epoch'], df['train/g_loss'], label='train/g_loss')
-x = df['train/d_loss']
+x = df['train/g_loss']
 x = x[~np.isnan(x)]
 ax.plot(x)
